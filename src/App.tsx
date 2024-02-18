415586d2028d40d9f0ceecbe7f3e24cbb38e9ca6
Pasha Mayba <pashamayba7@gmail.com>
2024-02-18 17:25:53 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import CategoryListPage from './category/list/CategoryListPage'
+import { Route, Routes } from 'react-router-dom'
+import DefaultLayout from './containers/DefaultLayout'
+import CategoryCreatePage from './category/create/CategoryCreatePage'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Routes>
+      <Route path={"/"} element={<DefaultLayout/>}>
+        <Route index element={<CategoryListPage/>}/>
+        <Route path={"category"}>
+                    <Route path = "create" element={<CategoryCreatePage/>}/>
+                </Route>
+      </Route>
+    </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/category/create/type.ts
@@ -0,0 +1,10 @@
+
+export interface ICategoryCreate {
+    name: string;
+    file: File|undefined;
+    description: string;
+}
+
+export interface IUploadedFile { 
+    originFileObj: File
+}
